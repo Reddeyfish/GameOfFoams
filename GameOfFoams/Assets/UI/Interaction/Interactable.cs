1bbbc250e29e87f0f98ece51516bc8b49c14c22d
Reddeyfish <edrichd@uci.edu>
2016-09-09 12:13:42 -0700

    Add functionality for interaction

---
+++ GameOfFoams/Assets/UI/Interaction/Interactable.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+
+public class Interactable : MonoBehaviour {
+
+    [SerializeField]
+    protected float interactionCooldown = 1;
+
+    [SerializeField]
+    protected float billboardHeightFromCenter = 1;
+
+    [SerializeField]
+    protected float billboardDepthFromCenter = 1;
+
+    private bool isInteractable = true;
+    public bool IsInteractable {
+        get { return isInteractable; }
+        private set { isInteractable = value; }
+    }
+
+    public delegate void InteractEvent();
+    public event InteractEvent InteractEventPublisher = delegate { };
+
+    public virtual void Position(Transform toPosition)
+    {
+        Vector3 viewDir = Camera.main.transform.position - transform.position;
+        viewDir.y = 0;
+        viewDir.Normalize();
+        toPosition.position = transform.position
+            + (billboardHeightFromCenter * Vector3.up)
+            + (billboardDepthFromCenter * viewDir);
+        toPosition.rotation = Quaternion.LookRotation(-viewDir, Vector3.up);
+    }
+
+    public void Interact()
+    {
+        Assert.IsTrue(IsInteractable);
+        IsInteractable = false;
+        Callback.FireAndForget(() => IsInteractable = true, interactionCooldown, this);
+        Debug.Log("Interact");
+        InteractEventPublisher();
+    }
+}
---
+++ GameOfFoams/Assets/UI/Interaction/PlayerInteraction.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))] //trigger
+public class PlayerInteraction : MonoBehaviour {
+
+    [SerializeField]
+    protected Transform InteractionUIPrefab;
+
+    IInput input;
+    Transform interactionUI;
+
+    /// <summary>
+    /// The closest interactable collider within our trigger. Null if one is not present.
+    /// </summary>
+    Collider target;
+    Interactable targetInteractable;
+
+
+	// Use this for initialization
+	void Start () {
+        input = GetComponentInParent<IInput>();
+        interactionUI = Instantiate(InteractionUIPrefab);
+        interactionUI.gameObject.SetActive(false);
+	}
+
+    void Update()
+    {
+        if (targetInteractable != null && targetInteractable.IsInteractable)
+        {
+            interactionUI.gameObject.SetActive(true);
+            targetInteractable.Position(interactionUI);
+            if (input.interacting)
+            {
+                targetInteractable.Interact();
+            }
+        }
+        else
+        {
+            interactionUI.gameObject.SetActive(false);
+        }
+    }
+
+    void SetTarget(Collider target, Interactable targetInteractable)
+    {
+        this.target = target;
+        this.targetInteractable = targetInteractable;
+        Assert.IsTrue(targetInteractable.IsInteractable);
+    }
+
+    void UnsetTarget()
+    {
+        target = null;
+        targetInteractable = null;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        OnTriggerStay(col);
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col == target)
+        {
+            return;
+        }
+
+        Interactable interactable = col.GetComponentInParent<Interactable>();
+        if (interactable == null)
+        {
+            return;
+        }
+
+        if (target == null || targetInteractable == null || !targetInteractable.IsInteractable || sqrDistance(col) < sqrDistance(target))
+        {
+            SetTarget(col, interactable);
+        }
+    }
+
+    float sqrDistance(Collider col)
+    {
+        return (col.transform.position - this.transform.position).sqrMagnitude;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (target == col)
+        {
+            UnsetTarget();
+        }
+    }
+}

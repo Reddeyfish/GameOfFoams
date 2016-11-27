c11d4342010c034bfc7eac51546609633fd68c0d
Reddeyfish <edrichd@uci.edu>
2016-11-27 12:05:29 -0800

    Add catapult trap

---
+++ GameOfFoams/Assets/Environment/Catapult/Catapult.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Catapult : MonoBehaviour, ITeamReference {
+
+    [SerializeField]
+    protected float radius;
+
+    [SerializeField]
+    protected float damage;
+
+    [SerializeField]
+    protected float delay;
+
+    [SerializeField]
+    protected GameObject impactVFX;
+
+    public Team Team { get { return Team.PLAYER; } }
+
+    void Start() {
+        GetComponent<Interactable>().InteractEventPublisher += OnInteract;
+    }
+
+    public void OnInteract() {
+        Callback.FireAndForget(() => {
+            foreach (Collider other in Physics.OverlapSphere(transform.position, radius)) {
+                EnemyNavigation enemy = other.transform.GetComponentInParent<EnemyNavigation>();
+                Rigidbody enemyRigidbody = other.transform.GetComponentInParent<Rigidbody>();
+                Health enemyHealth = other.transform.GetComponentInParent<Health>();
+
+                if (enemy == null || enemyRigidbody == null || enemyHealth == null) {
+                    continue; //hit a wall, or something
+                }
+
+                if (!enemyHealth.Damage(damage, this)) {
+                    continue; //target is invulnerable
+                }
+
+                Instantiate(impactVFX, other.transform.position, Quaternion.identity);
+            }
+        }, delay, this);
+    }
+}
---
+++ GameOfFoams/Assets/Environment/Catapult/CatapultBaseBuildingData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class CatapultBaseBuildingData : RotationBaseBuildingData {
+}
